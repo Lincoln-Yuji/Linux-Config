4006f5f854861b0f9a2b953555948b405e1b91e8
Lincoln Yuji <lincolnyuji@hotmail.com>
2022-05-05 21:44:23 -0300

    Create a simple script to install either dependencies or config files
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update && apt full-upgrade
+
+sudo apt install timeshift discord bashtop neofetch acpi;
+sudo apt install ibx11-dev libxcursor-dev ninja-build cmake;
+sudo apt install gettext libtool libtool-bin autoconf automake pkg-config unzip;
+sudo apt install nodejs npm;
