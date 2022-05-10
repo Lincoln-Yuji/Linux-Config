8acfda99f0b6173b6ede3444455de7f33a90a25b
Lincoln Yuji <lincolnyuji@hotmail.com>
2022-05-09 22:50:47 -0300

    Update the installation script
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

--- install.sh
+++ install.sh
@@ -6,3 +6,14 @@ sudo apt install timeshift discord bashtop neofetch acpi;
 sudo apt install ibx11-dev libxcursor-dev ninja-build cmake;
 sudo apt install gettext libtool libtool-bin autoconf automake pkg-config unzip;
 sudo apt install nodejs npm;
+
+# If we want to install latex, we don't want to make the full instaltion
+# This one is the recomended for us:
+sudo apt install texlive-latex-extra apt-file
+sudo apt-file update
+
+# If we need to install additional packages, we can search for them using the
+# apt-file command:
+# $ apt-file search <file-name>
+# This will find the pakages that conatain <file-name>, for example:
+# $ apt-file search titling.sty
