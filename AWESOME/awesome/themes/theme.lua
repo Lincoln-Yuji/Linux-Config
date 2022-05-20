6b2d13a7a1d5fc8fb123561084bc350f3d3d53fe
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2022-05-20 12:59:19 -0300

    Change themes
    Signed-off-by: Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>

--- AWESOME/awesome/themes/theme.lua
+++ AWESOME/awesome/themes/theme.lua
@@ -12,8 +12,8 @@ theme.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 theme.font = "Hack Nerd Font Bold 10"
 
 -- theme.wallpaper    = theme_dir .. "/wallpapers/galen-crout-175291.jpg"
-theme.wallpaper    = theme_dir .. "/wallpapers/pop.png"
-theme.awesome_icon = theme_dir .. "/icons/pop-os.png"
+-- theme.wallpaper    = theme_dir .. "/wallpapers/pop.png"
+theme.wallpaper    = theme_dir .. "/wallpapers/linux.png"
 
 theme.useless_gap = 2
 
