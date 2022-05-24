7a6f7ad9358f285d69bf04310c7c0b20ee1a8b01
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2022-05-24 15:22:50 -0300

    Force specifc clients to start on floating mode
    Signed-off-by: Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>

--- AWESOME/awesome/rc.lua
+++ AWESOME/awesome/rc.lua
@@ -256,7 +256,41 @@ awful.rules.rules = {
           screen    = awful.screen.preferred,
           placement = awful.placement.no_overlap+awful.placement.no_offscreen
        }
-    }
+    },
+
+    -- Floating clients.
+    { rule_any = {
+        instance = {
+            "DTA",  -- Firefox addon DownThemAll.
+            "copyq",  -- Includes session name in class.
+            "pinentry",
+        },
+        class = {
+            "Arandr",
+            "Blueman-manager",
+            "Gpick",
+            "Wpa_gui",
+            "veromix",
+            "xtightvncviewer",
+            "Gscreenshot",
+        },
+        -- Note that the name property shown in xprop might be set slightly after creation of the client
+        -- and the name shown there might not match defined rules here.
+        name = {
+            "Event Tester",  -- xev.
+        },
+        role = {
+            "AlarmWindow",    -- Thunderbird's calendar.
+            "ConfigManager",  -- Thunderbird's about:config.
+            "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
+        }
+    },
+        properties = { floating = true }
+    },
+
+    -- Set Firefox to always map on the tag named "2" on screen 1.
+    -- { rule = { class = "Firefox" },
+    --   properties = { screen = 1, tag = "2" } },
 }
 
 -- Signal function to execute when a new client appears.
