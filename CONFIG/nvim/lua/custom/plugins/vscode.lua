1c04bdb751d8dd8a344436b0c6effe4f4ce6da3b
Lincoln Yuji <lincolnyuji@hotmail.com>
2024-06-26 20:33:31 -0300

    nvim: Use prettier colors for DiffAdd and DiffDelete
    The default colorscheme is too hard to read. Using the ones
    defined with GitGutter is better and also prettier.
    
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

--- CONFIG/nvim/lua/custom/plugins/vscode.lua
+++ CONFIG/nvim/lua/custom/plugins/vscode.lua
@@ -4,7 +4,14 @@ return {
   'Mofiqul/vscode.nvim',
   priority = 1000,
   config = function()
+    -- Load colorscheme
     vim.cmd.colorscheme 'vscode'
+
+    -- Default GREEN VSCode color for comments is too distracting!
     vim.api.nvim_set_hl(0, '@comment', { fg = '#5A5A5A' })
+
+    -- Prettier highlight for Additions and Deletions for Git
+    vim.cmd [[ highlight! link DiffAdd GitGutterAdd ]]
+    vim.cmd [[ highlight! link DiffDelete GitGutterDelete ]]
   end,
 }
