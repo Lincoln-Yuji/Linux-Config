8a1d3dd78b3f664675b6d989b07e0e62e09fc714
Lincoln Yuji <lincolnyuji@hotmail.com>
2024-06-26 18:17:24 -0400

    Add new Neovim config
    The previous one BROKE! This new one will use Lazy
    
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

--- CONFIG/nvim/lua/setup-plugins/barbar.lua
+++
@@ -1,6 +0,0 @@
-local map = vim.api.nvim_set_keymap
-local opt = { noremap = true }
-
-map('n', '<Tab>',      '<cmd>BufferNext<CR>',     opt)
-map('n', '<S-Tab>',    '<cmd>BufferPrevious<CR>', opt)
-map('n', '<Leader>bc', '<cmd>BufferClose<CR>',    opt)
--- CONFIG/nvim/lua/setup-plugins/floaterm.lua
+++
@@ -1,6 +0,0 @@
--- Float Window proportions
-vim.g.floaterm_width = 0.85
-vim.g.floaterm_height = 0.9
-
--- Vplit when open new window
-vim.g.floaterm_opener = 'edit'
