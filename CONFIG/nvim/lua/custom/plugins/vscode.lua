cb8c92c8d33a12084558321540df16ba1f679eef
Lincoln Yuji <lincolnyuji@hotmail.com>
2024-06-28 19:19:24 -0300

    nvim: Link 'Comment' to '@comment'
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

--- CONFIG/nvim/lua/custom/plugins/vscode.lua
+++ CONFIG/nvim/lua/custom/plugins/vscode.lua
@@ -10,6 +10,11 @@ return {
     -- Default GREEN VSCode color for comments is too distracting!
     vim.api.nvim_set_hl(0, '@comment', { fg = '#5A5A5A' })
 
+    -- When '@comment' is not used due to lack of a Treesitter for
+    -- specific files, 'Comment' is the fallback, so it needs to be
+    -- changed as well following the '@comment'
+    vim.cmd [[ highlight! link Comment @comment ]]
+
     -- Prettier highlight for Additions and Deletions for Git
     vim.cmd [[ highlight! link DiffAdd GitGutterAdd ]]
     vim.cmd [[ highlight! link DiffDelete GitGutterDelete ]]
