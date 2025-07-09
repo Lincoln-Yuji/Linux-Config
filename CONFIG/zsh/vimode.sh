1d43036c211d590d0ec6b86e4621b0c133595d01
Lincoln Yuji <lincolnyuji@hotmail.com>
2025-07-09 12:54:48 -0300

    Put the ZSH vimode config in an aparted fie
    Putting the vimode confi for ZSH in a different file. Whenever
    we feel it's better to have it or not, we can simply source it
    or not inside zshrc
    
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

---
+++ CONFIG/zsh/vimode.sh
@@ -0,0 +1,22 @@
+# Enable vi mode
+bindkey -v
+bindkey "^?" backward-delete-char
+export KEYTIMEOUT=1
+
+# Changing the prompt cursor for different vi modes (Taken from Luke Smith video)
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q'
+preexec() { echo -ne '\e[5 q' ;}
