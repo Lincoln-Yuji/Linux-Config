f14f924e3349f09eae4e7818300677e7a023882f
Lincoln Yuji <lincolnyuji@hotmail.com>
2025-07-09 16:43:44 -0300

    Delete old shell scripts that I don't use anymore
    Those scripts used to exist so our window manager could call them,
    but now I'm just using KDE and don't have intentions to go back
    to window managers anymore.
    
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

--- SHELL-SCRIPTS/browser-bookmark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-link=$(grep http $HOME/.local/bin/meta/bookmarks | rofi -dmenu)
-
-if [ ! -z "$link" ]; then
-    $BROWSER ${link##* }
-fi
--- SHELL-SCRIPTS/script-pacupdates.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-
-echo "Updates: $(checkupdates | wc -l)"
