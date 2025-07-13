8202e4d90953094471e5c0bb5f43605a75540eb1
Lincoln Yuji <lincolnyuji@hotmail.com>
2025-07-13 19:59:53 -0300

    config: Update fzf commands
    Updating the 'Ctrl + t' command to use EZA when the entry is
    a directory, and BAT otherwise.
    
    Signed-off-by: Lincoln Yuji <lincolnyuji@hotmail.com>

--- CONFIG/zsh/fzf-config.sh
+++ CONFIG/zsh/fzf-config.sh
@@ -5,13 +5,16 @@
 # [Ctrl+t]: prompts *files* inside tree from current directory
 # [Alt+c]:  prompts *directories* inside tree from current directory
 
+declare file_preview="bat --theme='Visual Studio Dark+' --color=always -n --line-range :200"
+declare path_preview="eza -la --color=always --tree"
+
 export FZF_DEFAULT_COMMAND='fd --hidden --strip-cwd-prefix --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="${FZF_DEFAULT_COMMAND} --type=d"
 
 export FZF_DEFAULT_OPTS='--height 100% --layout=reverse --border --color=hl:#2dd4bf'
-export FZF_CTRL_T_OPTS="--preview \"bat --theme='Visual Studio Dark+' --color=always -n --line-range :200 {}\" --preview-window=down:50%"
-export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always -la {}' --preview-window=right:60%"
+export FZF_CTRL_T_OPTS="--preview \"[[ -d {} ]] && ${path_preview} {} || ${file_preview} {}\" --preview-window=down:55%"
+export FZF_ALT_C_OPTS="--preview '${path_preview} {}' --preview-window=right:60%"
 
 # Load fzf-zsh script extension
 eval "$(fzf --zsh)"
