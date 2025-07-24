0b68fd48f9315e4dc28d51deceab42756fc8da24
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2025-07-24 17:33:50 -0300

    Remove gruvbox theme from neovim config
    Signed-off-by: Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>

--- nvim/nvim/lua/custom/plugins/vscode.lua
+++ nvim/nvim/lua/custom/plugins/vscode.lua
@@ -1,13 +1,11 @@
 -- VSCode Color Scheme
 
 return {
-  -- 'Mofiqul/vscode.nvim',
-  "ellisonleao/gruvbox.nvim",
+  'Mofiqul/vscode.nvim',
   priority = 1000,
   config = function()
     -- Load colorscheme
-    -- vim.cmd.colorscheme 'vscode'
-    vim.cmd.colorscheme 'gruvbox'
+    vim.cmd.colorscheme 'vscode'
 
     -- Default GREEN VSCode color for comments is too distracting!
     -- vim.api.nvim_set_hl(0, '@comment', { fg = '#5A5A5A' })
