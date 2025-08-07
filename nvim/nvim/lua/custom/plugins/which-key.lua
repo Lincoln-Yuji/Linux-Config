42c321dea898e765b9d74174cf6b9e6b209483bf
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2025-08-06 21:21:05 -0300

    Update which-key config
    Applying syntax changes for the new version
    
    Signed-off-by: Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>

--- nvim/nvim/lua/custom/plugins/which-key.lua
+++ nvim/nvim/lua/custom/plugins/which-key.lua
@@ -1,27 +1,32 @@
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
 
-  return { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        -- [N]eotree is used to call Neotree actions
-        ['<leader>n'] = { name = '[N]eotree', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        -- [S]earch is used to call Telescope's functions
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        -- [W]orkspace is used for navigation between open windows
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- Visual Mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
-  }
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+
+  config = function() -- This is the function that runs, AFTER loading
+
+    require('which-key').setup()
+
+    -- Document existing key chains
+    require('which-key').add {
+      { "<leader>b", group = "[B]uffer" },
+      { "<leader>c", group = "[C]ode" },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>h", group = "Git [H]unk" },
+      { "<leader>n", group = "[N]eotree" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>t", group = "[T]oggle" },
+      { "<leader>w", group = "[W]orkspace" },
+    }
+  end,
+}
